ebcf28356d379c68d8fcc31ab19abedf38837451
Toshiki Watanabe <kuboy2482@gmail.com>
2020-07-25 20:34:12 +0900

    add comments

--- simple_rl/network/utils.py
+++ simple_rl/network/utils.py
@@ -29,11 +29,22 @@ def build_mlp(input_dim, output_dim, hidden_units=[64, 64],
 
 
 def calculate_gaussian_log_prob(log_stds, noises):
+    # NOTE: We only use multivariate gaussian distribution with diagonal
+    # covariance matrix,  which can be viewed as simultaneous distribution of
+    # gaussian distributions, p_i(u). So, log_probs = \sum_i log p_i(u).
     return (-0.5 * noises.pow(2) - log_stds).sum(dim=-1, keepdim=True) \
         - 0.5 * math.log(2 * math.pi) * log_stds.size(-1)
 
 
 def calculate_log_pi(log_stds, noises, us):
+    # NOTE: Because we calculate actions = tanh(us), we need to correct
+    # log_probs. Because tanh(u)' = 1 - tanh(u)**2, we need to substract
+    # \sum_i log(1 - tanh(u)**2) from gaussian_log_probs. For numerical
+    # stabilities, we use the deformation as below.
+    # log(1 - tanh(u)**2)
+    # = 2 * log(2 / (exp(u) + exp(-u)))
+    # = 2 * (log(2) - log(exp(u) * (1 + exp(-2*u))))
+    # = 2 * (log(2) - u - softplus(-2*u))
     return calculate_gaussian_log_prob(log_stds, noises) - (
         2 * (math.log(2) - us - F.softplus(-2 * us))
     ).sum(dim=-1, keepdim=True)
